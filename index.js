60fca0b1bc50a86fe30a7ed2455be3b7969bd9c6
Yozora <nightwoan@gmail.com>
2025-02-14 12:01:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,100 +1,36 @@
-// @ts-nocheck
-import { eventSource, event_types, saveSettingsDebounced, extension_settings } from "../../../../../script.js";
+import { eventSource, event_types, stopGeneration } from '../../../script.js';
+import { extension_settings, renderExtensionTemplateAsync } from '../../extensions.js';
+import { t } from '../../i18n.js';
 
-const extensionName = "JS-Slash-Runner-Quota-Check";
-const extensionFolderPath = `third-party/${extensionName}`;
+/**
+ * Checks the console output for "Out of quota" and stops generation if found.
+ */
+function checkConsoleOutput() {
+    const consoleOriginalError = console.error;
 
-const defaultSettings = {
-    activate_setting: true,
-};
-
-let originalConsoleLog = console.log;
-let originalConsoleWarn = console.warn;
-let originalConsoleError = console.error;
-
-function loadSettings() {
-    extension_settings[extensionName] = extension_settings[extensionName] || {};
-    if (Object.keys(extension_settings[extensionName]).length === 0) {
-        Object.assign(extension_settings[extensionName], defaultSettings);
-        saveSettingsDebounced();
-    }
-
-    $("#activate_setting").prop("checked", extension_settings[extensionName].activate_setting);
-}
-
-function checkForQuotaError(message) {
-    if (typeof message === 'string' && message.includes("Out of quota")) {
-        // 触发一个自定义事件来通知其他部分停止生成
-        eventSource.emit("stop_generation");
-        console.log("检测到 Out of quota，已触发停止生成事件");
-    }
-}
-
-function overrideConsoleMethods() {
-  console.log = function(...args) {
-        args.forEach(checkForQuotaError);
-        originalConsoleLog.apply(console, args);
-    };
-
-    console.warn = function(...args) {
-        args.forEach(checkForQuotaError);
-        originalConsoleWarn.apply(console, args);
-    };
-
-    console.error = function(...args) {
-        args.forEach(checkForQuotaError);
-        originalConsoleError.apply(console, args);
+    console.error = function (...args) {
+        const message = args.join(' ');
+        if (message.toLowerCase().includes('out of quota')) {
+            console.warn('Out of quota detected. Stopping generation.');
+            stopGeneration();
+            toastr.error(t`Out of quota detected. Generation stopped.`);
+        }
+        consoleOriginalError.apply(console, args);
     };
 }
 
-function restoreConsoleMethods() {
-    console.log = originalConsoleLog;
-    console.warn = originalConsoleWarn;
-    console.error = originalConsoleError;
-}
-
-async function onExtensionToggle() {
-    const isEnabled = Boolean($("#activate_setting").prop("checked"));
-    extension_settings[extensionName].activate_setting = isEnabled;
-
-    if (isEnabled) {
-        overrideConsoleMethods();
-    } else {
-        restoreConsoleMethods();
+jQuery(async () => {
+    // Manually disable the extension since static imports auto-import the JS file
+    if (extension_settings.disabledExtensions.includes('quota_check')) {
+        return;
     }
-    saveSettingsDebounced();
-}
 
+    const settingsHtml = $(await renderExtensionTemplateAsync('quota_check', 'dropdown'));
+    $('#quota_check_container').append(settingsHtml); //注意这里的ID，需要和manifest中的js文件名匹配
 
-async function renderSettings() {
-    const settingsHtml = $(`
-        <div class="js_slash_runner_quota_check_settings">
-            <div class="inline-drawer">
-                <div class="inline-drawer-toggle inline-drawer-header">
-                    <b>JS Slash Runner Quota Check</b>
-                    <div class="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
-                </div>
-                <div class="inline-drawer-content">
-                    <div class="flex-container">
-                        <label for="activate_setting">启用扩展:</label>
-                        <input id="activate_setting" type="checkbox" >
-                    </div>
-                </div>
-            </div>
-        </div>
-    `);
+    checkConsoleOutput();
 
-    $("#extensions_settings").append(settingsHtml);
-
-    $("#activate_setting").on("click", onExtensionToggle);
-   
-    loadSettings();
-}
-
-$(document).ready(function () {
-    renderSettings();  // 渲染设置界面
-     if (extension_settings[extensionName]?.activate_setting) {
-        overrideConsoleMethods();
-    }
+    // Add a listener for streaming start, so we can re-hook console.error
+    eventSource.on(event_types.STREAMING_STARTED, checkConsoleOutput);
 });
 
