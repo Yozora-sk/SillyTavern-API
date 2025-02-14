dc245a62a5228837200ab18b75432515bfc7f69b
Yozora <nightwoan@gmail.com>
2025-02-14 11:54:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 // @ts-nocheck
-import {
-    eventSource,
-    event_types,
-    saveSettingsDebounced,
-} from "../../../../../script.js";
-import {
-    extension_settings,
-    renderExtensionTemplateAsync,
-} from "../../../../extensions.js";
+import { eventSource, event_types, saveSettingsDebounced, extension_settings } from "../../../../../script.js";
 
 const extensionName = "JS-Slash-Runner-Quota-Check";
 const extensionFolderPath = `third-party/${extensionName}`;
@@ -30,7 +22,6 @@ function loadSettings() {
     $("#activate_setting").prop("checked", extension_settings[extensionName].activate_setting);
 }
 
-
 function checkForQuotaError(message) {
     if (typeof message === 'string' && message.includes("Out of quota")) {
         // 触发一个自定义事件来通知其他部分停止生成
@@ -39,19 +30,18 @@ function checkForQuotaError(message) {
     }
 }
 
-
 function overrideConsoleMethods() {
-    console.log = function (...args) {
+  console.log = function(...args) {
         args.forEach(checkForQuotaError);
         originalConsoleLog.apply(console, args);
     };
 
-    console.warn = function (...args) {
+    console.warn = function(...args) {
         args.forEach(checkForQuotaError);
         originalConsoleWarn.apply(console, args);
     };
 
-    console.error = function (...args) {
+    console.error = function(...args) {
         args.forEach(checkForQuotaError);
         originalConsoleError.apply(console, args);
     };
@@ -77,19 +67,34 @@ async function onExtensionToggle() {
 
 
 async function renderSettings() {
-    const template = await renderExtensionTemplateAsync(extensionFolderPath, 'settings');
-    $('#extensions_settings').append(template);
+    const settingsHtml = $(`
+        <div class="js_slash_runner_quota_check_settings">
+            <div class="inline-drawer">
+                <div class="inline-drawer-toggle inline-drawer-header">
+                    <b>JS Slash Runner Quota Check</b>
+                    <div class="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
+                </div>
+                <div class="inline-drawer-content">
+                    <div class="flex-container">
+                        <label for="activate_setting">启用扩展:</label>
+                        <input id="activate_setting" type="checkbox" >
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
 
+    $("#extensions_settings").append(settingsHtml);
+
+    $("#activate_setting").on("click", onExtensionToggle);
+   
     loadSettings();
-
-
-    $("#activate_setting").click(onExtensionToggle);
-
-    // Add quota check on document ready
-    if (extension_settings[extensionName]?.activate_setting) {
-        overrideConsoleMethods();
-    }
 }
 
-renderSettings();
+$(document).ready(function () {
+    renderSettings();  // 渲染设置界面
+     if (extension_settings[extensionName]?.activate_setting) {
+        overrideConsoleMethods();
+    }
+});
 
